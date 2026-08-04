b2999233e6c840a34c57b93b379737a0f6697b75
Ferdavid890 <ferdavid890@outlook.com>
2026-08-04 15:23:01 -0600

    Update app.py

--- app.py
+++ app.py
@@ -53,9 +53,10 @@ def obtener_token_ebay():
         "Content-Type": "application/x-www-form-urlencoded",
         "Authorization": f"Basic {encoded_credentials}"
     }
+    
+    # CORREGIDO: Se remueve el scope incorrecto para que tome el valor por defecto de la App
     body = {
-        "grant_type": "client_credentials",
-        "scope": "https://api.ebay.com/oauth/api_scope"
+        "grant_type": "client_credentials"
     }
 
     response = requests.post(url, headers=headers, data=body)
